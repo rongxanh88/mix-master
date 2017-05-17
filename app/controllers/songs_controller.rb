def4971e9723fc40e46fac84d7ecbfb8a067fc41
rongxanh88 <rongxanh88@gmail.com>
2017-05-17 13:37:34 -0600

    finish song iteration

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -5,7 +5,7 @@ class SongsController < ApplicationController
   end
 
   def create
-    artist = Artist.find(params[:song][:artist_id])
+    artist = Artist.find(params[:artist_id])
     @song = artist.songs.create!(song_params)
     redirect_to artist_path(@song.artist)
   end
